82710a0952fe771c1407d155a5bdfa504efb1f08
Tony de Padua <tonydpadua1999@gmail.com>
2020-02-25 18:10:22 -0300

    Department entities class

---
+++ src/Application/Program.java
@@ -0,0 +1,15 @@
+package Application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Department dep=new Department(1,"Books");
+		
+		System.out.println(dep);
+
+	}
+
+}
