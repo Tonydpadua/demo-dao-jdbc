36d6fa6033cf0ed7da7adffa3dd2857d0b25c1ed
Tony de Padua <tonydpadua1999@gmail.com>
2020-02-27 22:32:23 -0300

    findAll

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -16,23 +16,30 @@ public class Program {
 		
 		System.out.println("===TEST findById===");
 		Department dep=new Department(1,"Books");
-		
-		
-		
 		Seller seller=new Seller(2, "Bob", "bob@gmail.com", new Date(), 1000, dep);
-		
-		
 
 		SellerDao sd=DaoFactory.createSellerDao();
 		
+		System.out.println();
+		System.out.println("===TEST findById===");
 		Seller seller1=sd.findById(3);
 		System.out.println(seller1);
 		
+		System.out.println();
 		System.out.println("===TEST findByDepartment===");
 		List<Seller> list=sd.findByDepartmentId(dep);
 		for(Seller obj:list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println();
+		System.out.println("==TEST findAll===");
+		list=sd.findAll();
+		for(Seller obj:list) {
+			System.out.println(obj);
+		}
 	}
+	
+	
 
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -94,8 +94,45 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
+		PreparedStatement st=null;
+		ResultSet rs=null;
+		try {
+			st=conn.prepareStatement("select seller .*, department.Name as DepName \r\n" + 
+					" from seller INNER JOIN department \r\n" + 
+					" on seller.DepartmentId=department.Id\r\n" +  
+					" order by Name");
+			//st.setInt(1, department.getId());
+			rs=st.executeQuery();
+			List<Seller> list=new ArrayList<>();
+			Map<Integer,Department> map=new HashMap<>();
+			while(rs.next()) {
+				Department dep=map.get(rs.getInt("DepartmentId"));
+				if(dep==null) {
+					dep=instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"),dep);
+					
+				}
+			
+				
+				Seller obj=instantiateSeller(rs,dep);
+				list.add(obj);
+				
+				
+			}
+			return list;
+			
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+			
+			
+		}
 		return null;
+
 	}
 
 	@Override
